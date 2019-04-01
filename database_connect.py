89215f403aad188ac05150f660ce58f2034a5d6f
sauravray2587 <sauravray2587@gmail.com>
2019-04-01 17:49:16 +0530

    <

--- database_connect.py
+++ database_connect.py
@@ -26,23 +26,19 @@ def check_login(username, password):
     query = ("SELECT password FROM User"
              " WHERE username = %s ")
     cursor.execute(query, (username,))
-    print("size, " ,cursor.arraysize)
     if cursor.arraysize==0:
         print("User doesn't exist")
         return False
 
     for (saved_password,) in cursor:
         if password == saved_password:
-            print(password)
             return True
         else:
             print("Password not matched password = ", password, "actual password = ",saved_password)
             return False
 
 
-
-
 if __name__=="__main__":
     # sign_up("fsociety00","Shubham","say_hi", 21,"fsociety@gmail.com")
-    print(check_login("fsociety00","say_hi"))
-    # user_post("3" , "fsociety00", "high level content")
+    # print(check_login("fsociety00","say_hi"))
+    user_post("3" , "fsociety00", "high level content")
