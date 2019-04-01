dd405c4ae1f4800daab323c8280c8817d5268316
prashik-sa <prashiksahare110044@gmail.com>
2019-04-01 22:10:05 +0530

    added tag search

--- search_queries.py
+++ search_queries.py
@@ -16,18 +16,16 @@ cnx = mysql.connector.connect(user='root', password=pw,
                                   database=dbase)
 cursor = cnx.cursor(buffered=True)
 
-def search_tags(taglist):
+def search_tags(tag):
 
-	var_string = ', '.join('?' * len(taglist))
-	print(var_string)
-	query = ("SELECT * FROM Post WHERE \
-			post_id in (SELECT post_id FROM Post_tags \
-				WHERE tag_id in (%s))"% var_string)
-	cursor.execute(query, (taglist,))
+	query = """SELECT * FROM Post WHERE 
+			post_id in (SELECT post_id FROM Post_tags 
+				WHERE tag_id = %s)"""
+	cursor.execute(query, (tag,))
 
 	return get_posts(cursor)
 
 
 
 if __name__=="__main__":
-	print(search_tags(["dp","greedy"]))
+	print(search_tags("greedy"))
