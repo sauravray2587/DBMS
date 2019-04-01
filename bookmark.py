36b9af6d140979e093b2523e9e02e55202f119d2
prashik-sa <prashiksahare110044@gmail.com>
2019-04-01 19:43:50 +0530

    added feed

---
+++ bookmark.py
@@ -0,0 +1,23 @@
+import mysql.connector
+from stories import search_posts
+import operator
+import os
+
+if (os.getlogin()=='saurav'):
+    pw = 'qwerty@123'
+else:
+    pw = 'qw'
+
+dbase = 'web'
+
+cnx = mysql.connector.connect(user='root', password=pw,
+                                  host='127.0.0.1',
+                                  database=dbase)
+cursor = cnx.cursor(buffered=True)
+
+def bookmark(username, post_id):
+
+	query = ("INSERT INTO Bookmark \
+		VALUES( '%s', '%s')" %(username,post_id)
+	cursor.execute(query)
+	cnx.commit()
