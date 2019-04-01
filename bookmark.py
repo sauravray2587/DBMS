41dbd511aa5a111f17dbdee3a5e9724cee753501
prashik-sa <prashiksahare110044@gmail.com>
2019-04-02 04:56:55 +0530

    added feed in get_prerequisites

---
+++ bookmark.py
@@ -0,0 +1,31 @@
+import mysql.connector
+import operator
+import os
+
+if (os.environ['USER']=='saurav'):
+    pw = 'qwerty@123'
+else:
+    pw = 'qw'
+
+dbase = 'web'
+
+cnx = mysql.connector.connect(user='root', password=pw,
+                                  host='127.0.0.1',
+                                  database=dbase)
+cursor = cnx.cursor(buffered=True)
+
+def bookmark(username, post_id):
+
+	query = ("INSERT INTO Bookmark VALUES( %s, %s)")
+	cursor.execute(query, (username, post_id))
+	cnx.commit()
+
+def unbookmark(username, post_id):
+
+	query = ("delete from Bookmark where username = %s and post_id = %s")
+	cursor.execute(query, (username, post_id))
+
+	cnx.commit()
+
+if __name__ == '__main__':
+	unbookmark('piyushrathipr', 32)
--- prerequisite.py
+++ prerequisite.py
@@ -4,6 +4,7 @@ from config import connect_database
 
 cnx = connect_database()
 cursor = cnx.cursor(buffered=True)
+cursor1 = cnx.cursor(buffered=True)
 
 def create_prerequisite(post_id1, post_id2):
 	query = "INSERT INTO Prerequisite VALUES (%s, %s)"
@@ -11,14 +12,19 @@ def create_prerequisite(post_id1, post_id2):
 	cnx.commit()
 
 
-def get_prerequisites(post_id):
+def get_prerequisites(cur_user, post_id):
 	query = "SELECT post_id_2 FROM Prerequisite\
 			WHERE post_id_1 = %s"
 	cursor.execute(query,(post_id,))
 
 	prerequisites = []
 	for (prerequisite_id,) in cursor:
-		prerequisites.append(prerequisite_id)
+		query = ("SELECT post_id, username, content, rating, community_id, post_time FROM Post"
+			 " WHERE post_id = %s ")
+
+		cursor1.execute(query, (prerequisite_id,))
+
+		prerequisites.extend(get_post(cursor1,cur_user))
 	return prerequisites
 
 if __name__=="__main__":
